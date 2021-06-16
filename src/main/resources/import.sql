9ef8f891b4f4bc4519849fa23821d6f7a82434f8
João Paulo <jptick@gmail.com>
2021-06-16 14:42:01 -0300

    Mapping Entity Estado

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,3 +4,7 @@ insert into cozinha (nome) values ('Indiana');
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10, 1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Delivery', 9.50, 1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana', 15, 2);
+
+insert into estado (id, nome) values (1, 'Minas Gerais');
+insert into estado (id, nome) values (2, 'São Paulo');
+insert into estado (id, nome) values (3, 'Ceará');
