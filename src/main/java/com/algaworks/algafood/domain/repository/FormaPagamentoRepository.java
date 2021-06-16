7d74cfe5e470c3c451494dee09c27202b08f6bb5
João Paulo <jptick@gmail.com>
2021-06-16 15:10:24 -0300

    Mapping Entity Forma Pagamento

---
+++ src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
@@ -0,0 +1,7 @@
+package com.algaworks.algafood.domain.repository;
+
+import com.algaworks.algafood.domain.model.FormaPagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
+}
