67a5ae3efad42dd2e6713a0ed3195d11c6cf3e4d
João Paulo <jptick@gmail.com>
2021-06-16 15:05:57 -0300

    Mapping Entity Cidade

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,3 +8,9 @@ insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida I
 insert into estado (id, nome) values (1, 'Minas Gerais');
 insert into estado (id, nome) values (2, 'São Paulo');
 insert into estado (id, nome) values (3, 'Ceará');
+
+insert into cidade (id, nome, estado_id) values (1, 'Uberlândia', 1);
+insert into cidade (id, nome, estado_id) values (2, 'Belo Horizonte', 1);
+insert into cidade (id, nome, estado_id) values (3, 'São Paulo', 2);
+insert into cidade (id, nome, estado_id) values (4, 'Campinas', 2);
+insert into cidade (id, nome, estado_id) values (5, 'Fortaleza', 3);
