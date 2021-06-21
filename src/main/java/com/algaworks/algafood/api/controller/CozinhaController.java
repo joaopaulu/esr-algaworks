037d8a3343de38684b680f51da72b1482d6e1a68
João Paulo <jptick@gmail.com>
2021-06-21 16:46:07 -0300

    Implements Cozinha FindbyId

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -4,10 +4,12 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cozinhas")
@@ -19,5 +21,10 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listar(){
         return cozinhaRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Optional<Cozinha> buscar(@PathVariable Long id){
+        return cozinhaRepository.findById(id);
     }
 }
