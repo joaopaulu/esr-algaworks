e6cd9ee8dc77551d5062113f37f9bbd6854bf893
João Paulo Lima <jptick@gmail.com>
2021-11-17 16:55:11 -0300

    Using Query Methods

--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -4,9 +4,11 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
-    // List<Cozinha> consultarPorNome(String nome);
+    List<Cozinha> findByNomeContaining(String nome);
 
 }
