7c520b5380bad4abd65e0fbc9d4e3b685d3fa94e
João Paulo <jptick@gmail.com>
2021-12-08 18:21:55 -0300

    Create Entities Pedido, ItemPedido and StatusPedido

---
+++ src/main/java/com/algaworks/algafood/domain/model/ItemPedido.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class ItemPedido {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal preocoUnitario;
+    private BigDecimal precoTotal;
+    private Integer quantidade;
+    private String observacao;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Produto produto;
+}
