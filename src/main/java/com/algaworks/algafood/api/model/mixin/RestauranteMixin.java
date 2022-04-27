ec95d3e46d3a1fa41c4d42ae82225757e0711d4d
João Paulo Lima <jptick@gmail.com>
2022-04-27 10:12:34 -0300

    Create Cidade and Cozinha Jackson Mixin

--- src/main/java/com/algaworks/algafood/api/model/mixin/RestauranteMixin.java
+++ src/main/java/com/algaworks/algafood/api/model/mixin/RestauranteMixin.java
@@ -1,23 +1,13 @@
 package com.algaworks.algafood.api.model.mixin;
 
-import com.algaworks.algafood.core.validation.Groups;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Endereco;
 import com.algaworks.algafood.domain.model.FormaPagamento;
 import com.algaworks.algafood.domain.model.Produto;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.ToString;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.*;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.groups.ConvertGroup;
-import javax.validation.groups.Default;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 public class RestauranteMixin {
@@ -35,8 +25,8 @@ public class RestauranteMixin {
     private LocalDateTime dataAtualizacao;
 
     @JsonIgnore
-    private List<FormaPagamento> formasPagamento = new ArrayList<>();
+    private List<FormaPagamento> formasPagamento;
 
     @JsonIgnore
-    private List<Produto> produtos = new ArrayList<>();
+    private List<Produto> produtos;
 }
--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -28,7 +28,6 @@ public class Cozinha {
     @Column(nullable = false)
     private String nome;
 
-    @JsonIgnore
     @OneToMany(mappedBy = "cozinha")
     private List<Restaurante> restaurantes = new ArrayList<>();
 
