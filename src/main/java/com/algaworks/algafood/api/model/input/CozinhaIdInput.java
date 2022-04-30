556887a3b4754e33397dde4d494569946a2c75b0
João Paulo <jptick@gmail.com>
2022-04-29 22:07:33 -0300

    Create DTO to save Data in API

---
+++ src/main/java/com/algaworks/algafood/api/model/input/CozinhaIdInput.java
@@ -0,0 +1,5 @@
+package com.algaworks.algafood.api.model.input;
+
+public class CozinhaIdInput {
+    private Long id;
+}
---
+++ src/main/java/com/algaworks/algafood/api/model/input/RestauranteInput.java
@@ -0,0 +1,9 @@
+package com.algaworks.algafood.api.model.input;
+
+import java.math.BigDecimal;
+
+public class RestauranteInput {
+    private String nome;
+    private BigDecimal taxaFrete;
+    private CozinhaIdInput cozinha;
+}
